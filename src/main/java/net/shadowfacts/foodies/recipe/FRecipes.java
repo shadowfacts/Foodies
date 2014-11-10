3ed2dee3c1512d011169c989765b019fbbf4708f
Shadowfacts <akorde@icloud.com>
2014-11-09 21:31:31 -0500

    Add recipe helper class

---
+++ src/main/java/net/shadowfacts/foodies/recipe/FRecipes.java
@@ -0,0 +1,19 @@
+package net.shadowfacts.foodies.recipe;
+
+/**
+ * A helper class for registering recipes.
+ * @author shadowfacts
+ */
+public class FRecipes {
+	public static void preInit() {
+
+	}
+
+	public static void load() {
+
+	}
+
+	public static void postInit() {
+
+	}
+}
