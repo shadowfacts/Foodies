289a9b92d1c4a5828c00a017d0bd54d8bf7c4ea5
Shadowfacts <akorde@icloud.com>
2014-11-18 20:39:29 -0500

    Add hamburger

--- src/main/java/net/shadowfacts/foodies/item/FItems.java
+++ src/main/java/net/shadowfacts/foodies/item/FItems.java
@@ -11,17 +11,20 @@ public class FItems {
 	public static Food toast;
 	public static Food tomato;
 	public static Food beefPattie;
+	public static Food hamburger;
 
 	public static void preInit() {
 		// Create Items
 		toast = new Food(5, 0.3f).setUnlocalizedName("foodToast").setTextureName("foodToast");
 		tomato = new Food(1, 0.1f).setUnlocalizedName("fruitTomato").setTextureName("fruitTomato");
 		beefPattie = new Food(7, 0.4f).setUnlocalizedName("foodBeefPattie").setTextureName("foodBeefPattie");
+		hamburger = new Food(5, 0.7f).setUnlocalizedName("foodHamburger").setTextureName("foodHamburger");
 
 		// Register items
 		GameRegistry.registerItem(toast, "foodToast");
 		GameRegistry.registerItem(tomato, "fruitTomato");
 		GameRegistry.registerItem(beefPattie, "foodBeefPattie");
+		GameRegistry.registerItem(hamburger, "foodHamburger");
 	}
 
 	public static void load() {
