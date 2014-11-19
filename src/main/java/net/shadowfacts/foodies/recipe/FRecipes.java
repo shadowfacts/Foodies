0afbd19a7750d5b8dd45a8aeed0e5be0df88b26e
Shadowfacts <akorde@icloud.com>
2014-11-18 20:53:38 -0500

    Add recipes for new things

--- src/main/java/net/shadowfacts/foodies/recipe/FRecipes.java
+++ src/main/java/net/shadowfacts/foodies/recipe/FRecipes.java
@@ -11,7 +11,22 @@ import net.shadowfacts.foodies.item.FItems;
  */
 public class FRecipes {
 	public static void preInit() {
-		GameRegistry.addSmelting(Items.bread, new ItemStack(FItems.toast, 1), 0.2f);
+		// Shapeless
+		GameRegistry.addShapelessRecipe(new ItemStack(FItems.hamburger), new ItemStack(FItems.toast), new ItemStack(FItems.beefPattie), new ItemStack(FItems.toast));
+		GameRegistry.addShapelessRecipe(new ItemStack(FItems.cheeseburger), new ItemStack(FItems.hamburger), new ItemStack(FItems.cheese));
+
+		// Shaped
+
+
+
+
+		registerSmelting();
+	}
+
+	private static void registerSmelting() {
+		// Smelting
+		GameRegistry.addSmelting(Items.bread, new ItemStack(FItems.toast), 0.2f);
+		GameRegistry.addSmelting(Items.cooked_beef, new ItemStack(FItems.beefPattie), 0.3f);
 	}
 
 	public static void load() {
